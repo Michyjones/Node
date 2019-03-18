0a01e10f1f1a3678c3c05ef573c1895cf68edff3
Michyjones <mykembuguah11@gmail.com>
2019-03-18 10:53:42 +0300

    [Feature] Add body parser and a post route for creating an employee

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3000;
 const settings = require('./settings');
 const middlewares = require('./middlewares');
+const bodyParser = require('body-parser')
+
+const jsonParser = bodyParser.json();
 
 const knex = require('knex')({
   client: 'mysql',
@@ -17,6 +20,7 @@ const router = express.Router();
 
 router.get('/employees', routes.employees.AllEmployees);
 router.get('/employees/:id', middlewares.getIDAsInt, routes.employees.SingleEmployees);
+router.post('/employees', jsonParser ,routes.employees.CreateEmployees);
 
 app.use('/api', router);
 
